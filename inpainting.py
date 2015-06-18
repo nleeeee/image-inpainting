954137193a2af919593b11c1f4a916c04bd010ad
mknicklee <op@localhost>
2015-06-18 18:59:10 -0400

    added copy_patch, paste_patch, update confidence_image and mask

--- inpainting.py
+++ inpainting.py
@@ -10,29 +10,12 @@ from PIL import Image
 from scipy import ndimage
 from canny import *
 
-def compute_normal(boundary_pt, fill_front, patch_size = 9):
-    
-    return 1
-    
-def compute_data(boundary_pt, alpha, gradient, front_normal, patch_size = 9):
-    
-    x = boundary_pt[0]
-    y = boundary_pt[1]
-    pp = np.arange(-1*patch_size, patch_size+1)
-    data = 0
-    
-    for p in pp:
-        for q in pp:
-            data += np.dot(gradient[x+p,y+q], front_normal[x+p,y+q])
-    data /= alpha
-    return data    
-    
-def compute_confidence(boundary_ptx, boundary_pty, confidence_image, patch_size = 9):
+def find_max_priority(boundary_ptx, boundary_pty, src, patch_size = 9):
     
     max = np.sum(get_patch(boundary_ptx[0], boundary_pty[0], confidence_image))/patch_size
     i = 0
     while i < len(boundary_ptx):
-        curr_patch = get_patch(boundary_ptx[i],boundary_pty[i], confidence_image)
+        curr_patch = get_patch(boundary_ptx[i],boundary_pty[i], src)
         curr_max = np.sum(curr_patch)/patch_size
         if curr_max > max:
             max = curr_max
@@ -48,58 +31,88 @@ def get_patch(cntr_ptx, cntr_pty, img, patch_size = 9):
     p = patch_size // 2
     return img[x-p:x+p+1, y-p:y+p+1]
     
-def ssd(patch1, patch2):
-
-    return np.sum((patch1.flatten() - patch2.flatten()) ** 2)
+def patch_ssd(patch1, patch2):
     
-def find_exemplar_patch(src, patch, patch_size = 9L):
-    
+    len = patch1.flatten().shape[0]
     i = 0
-    j = 0
+    patch1 = patch1.flatten()
+    patch2 = patch2.flatten()
+    sum = 0
+    while i <= len - 1:
+        if patch1[i] != 0.1111:
+            sum += (patch1[i] - patch2[i]) ** 2
+        i += 1
+    return sum
     
-    xx = np.where(src != 0.1111)[0]
-    yy = np.where(src != 0.1111)[1]
-    print np.where(src != 0.1111)[0].shape
-    best = 0
+def find_exemplar_patch(src, x, y, patch, patch_size = 9L):
+    
+    i = 0    
+    filled = np.where(src != 0.1111)
+    xx = filled[0]
+    yy = filled[1]
+    initialized = 0
     
     while i < len(xx) - 1:
-        exemplar_patch = get_patch(xx[i], yy[j], src)
-        print i,j
-        if exemplar_patch.shape[0] == patch_size and exemplar_patch.shape[1] == patch_size:
-            if best == 0:
-                best_patch = ssd(get_patch(xx[i], yy[j], src), patch)
-                best = 1
-            if ssd(exemplar_patch, patch) <= best_patch:
-                best_patch = ssd(exemplar_patch, patch)
-        i = i + 1
-        j = j + 1
-    return xx[i], yy[j]
+        exemplar_patch = get_patch(xx[i], yy[i], src)
+        #print i,j
+        if exemplar_patch.flatten().shape[0] == 81:
+            if xx[i] != x and yy[i] != y and np.where(src==0.1111)[0].shape[0] == 0:
+                if initialized == 0:
+                    min_ssd = patch_ssd(get_patch(xx[i], yy[i], src), patch)
+                    exemplar_patch = get_patch(xx[i], yy[i], src)
+                    best_patch = exemplar_patch
+                    initialized = 1
+                ssd = patch_ssd(exemplar_patch, patch)
+                if ssd <= min_ssd:
+                    best_patch = exemplar_patch
+                    x = filled[0][i]
+                    y = filled[1][i]
+                    min_ssd = ssd
+                    print min_ssd,x,y
+        i += 1
+    return best_patch, x, y
     
-def find_max_priority(src, boundary_ptx, boundary_pty):
+def copy_patch(patch1, patch2):
     
+    unfilled = np.where(patch1 == 0.1111)
+    xx = unfilled[0]
+    yy = unfilled[1]
     i = 0
-    j = 0
-    max = np.sum(get_patch(boundary_ptx[i], boundary_pty[j], src)
-    return 1
-    
-def inpainting(region, size):
-    
-    return 1
 
+    while i <= len(xx) - 1:
+        patch1[xx[i]][yy[i]] = patch2[xx[i]][yy[i]]
+        i += 1
+        
+    return patch1
+    
+def paste_patch(x, y, patch, img, patch_size = 9):
+    
+    p = patch_size // 2
+    img[x-p:x+p+1, y-p:y+p+1] = patch
+    return img
+    
+def update(x, y, confidence_image, mask, patch_size = 9):
+    
+    p = patch_size // 2
+    confidence_image[x-p:x+p+1, y-p:y+p+1] = 1
+    mask[x-p:x+p+1, y-p:y+p+1] = 1
+    return confidence_image, mask
+    
 if __name__ == '__main__':
     
     src = imread('input.jpg')
+    src2 = src/255.0
     mask = imread('input-mask.bmp')
-    fill_front = canny(mask, 3)
+    mask /= 255.0
+    src2[np.where(mask==0)] = 0.1111
     
     # initialize confidence
     confidence_image = zeros(mask.shape)
     confidence_image[np.where(mask != 0)] = 1
     
-    unfilled = mask
+    # todo: create loop here
     grayscale = src[:,:,0]*.229 + src[:,:,1]*.587 + src[:,:,2]*.114
-    #grayscale = canny(grayscale, 3, 50, 10)
-    
+    fill_front = canny(mask, 1)
     boundary_ptx = np.where(fill_front > 0)[0]
     boundary_pty = np.where(fill_front > 0)[1]
     
@@ -108,15 +121,19 @@ if __name__ == '__main__':
     dy = ndimage.sobel(grayscale, 1)
     grad = np.hypot(dx, dy) # gradient
     norm = np.hypot(-dy, dx) # normal
-    # np.max(np.linalg.eig(np.dot(np.conjugate(norm).transpose(),norm))[0])
-    grad[np.where(mask == 0)] = 0.1111  
-    grad[np.where(fill_front > 0)] = 0.1111
+    grad[np.where(mask == 0)] = 0
+    grad[np.where(fill_front > 0)] = 0
     
-    a = compute_confidence(boundary_ptx, boundary_pty, confidence_image)
+    grayscale /= 255.0
     grayscale[np.where(mask == 0)] = 0.1111
     xx = np.where(grayscale != 0.1111)[0]
     yy = np.where(grayscale != 0.1111)[1]
     
-    #np.linalg.qr(grad)[0]
-    #priority_image = 
-    #find_exemplar_patch(grayscale, get_patch(61,35,grayscale))
+    data_term = np.linalg.qr(grad)[0]*norm
+    priority_image = confidence_image*data_term
+    highest_priority = find_max_priority(boundary_ptx, boundary_pty, priority_image)
+    best_patch = find_exemplar_patch(grayscale,highest_priority[1],highest_priority[2], get_patch(highest_priority[1],highest_priority[2],grayscale))
+    c = copy_patch(get_patch(highest_priority[1],highest_priority[2],src2),get_patch(best_patch[1],best_patch[2],src2)) #test 
+    paste_patch(highest_priority[1],highest_priority[2],c,src2) #test
+    confidence_image, mask = update(highest_priority[1],highest_priority[2],confidence_image,mask) #test
+    
