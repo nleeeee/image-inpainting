1723672eee321db4f1c0ef9bd5a44ebfa2381438
mknicklee <mknicklee@gmail.com>
2015-08-13 17:44:00 -0400

    added option to set patch size

--- InpaintingGUI.py
+++ InpaintingGUI.py
@@ -1,15 +1,22 @@
-#!/usr/bin/python
-
 import os
 import wx
 from inpainting import inpaint
 
 class InpaintingGUI(wx.Frame):
+    
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, wx.DefaultPosition, wx.Size(512,390))
+        wx.Frame.__init__(self, 
+                          parent, 
+                          id, 
+                          title, 
+                          wx.DefaultPosition, 
+                          wx.Size(512,390))
         self.dirname = ''
         self.img = ''
         self.mask = ''
+        self.patch_size = 9 # default patch size
+        self.gauss = 0 # no Gaussian smoothing by default
+        self.sigma = 1 # sigma value for Gaussian smoothing
         self.CreateStatusBar()
         menubar = wx.MenuBar()
         file = wx.Menu()
@@ -19,14 +26,16 @@ class InpaintingGUI(wx.Frame):
         imageOpen = wx.MenuItem(file, 101, '&Open Image\tCtrl+O', 'Open image')
         maskOpen = wx.MenuItem(file, 102, '&Open Mask\tCtrl+M', 'Open mask')
         quit = wx.MenuItem(file, 105, '&Quit\tCtrl+Q', 'Quit the Application')
-        pref = wx.MenuItem(settings, 201, '&Settings', 'Change parameters')
+        patchSize = wx.MenuItem(settings, 201, '&Patch Size', 'Change patch size')
+        gauss = wx.MenuItem(settings, 202, '&Gaussian Smoothing', '')
         aboutInfo = wx.MenuItem(help, 301, '&About', '')
         
         file.AppendItem(imageOpen)
         file.AppendItem(maskOpen)
         file.AppendSeparator()
         file.AppendItem(quit)
-        settings.AppendItem(pref)
+        settings.AppendItem(patchSize)
+        settings.AppendItem(gauss)
         help.AppendItem(aboutInfo)
         
         menubar.Append(file, '&File')
@@ -42,6 +51,8 @@ class InpaintingGUI(wx.Frame):
         self.Bind(wx.EVT_MENU, self.onOpenImage, id=101)
         self.Bind(wx.EVT_MENU, self.onOpenMask, id=102)
         self.Bind(wx.EVT_MENU, self.onAbout, id=301)
+        self.Bind(wx.EVT_MENU, self.onPatchSize, id=201)
+        #self.Bind(wx.EVT_MENU, self.onGauss, id=202)
         self.Bind(wx.EVT_MENU, self.onQuit, id=105)
         
         self.SetMenuBar(menubar)
@@ -49,20 +60,22 @@ class InpaintingGUI(wx.Frame):
         self.Show()
 
     def onQuit(self, e):
+        '''Closes GUI'''
         self.Close()
         
     def onAbout(self,e):
-        # Create a message dialog box
+        '''Opens About dialog'''
         dlg = wx.MessageDialog(self, 'Image Inpainting', 'About', wx.OK)
-        dlg.ShowModal() # Shows it
-        dlg.Destroy() # finally destroy it when finished.
+        dlg.ShowModal()
+        dlg.Destroy()
     
     def onOpenImage(self, e):
-        '''Open a mask'''
+        '''Open image to inpaint'''
         wildcard = 'JPEG files (*.jpg)|*.jpg|' +\
                    'PNG files (*.png)|*.png|' +\
                    'Other files (*.*)|*.*'
-        dlg = wx.FileDialog(self, "Choose an image", wildcard=wildcard, style=wx.OPEN)
+        dlg = wx.FileDialog(self, "Choose an image", 
+                            wildcard=wildcard, style=wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
             self.filename = dlg.GetFilename()
             self.dirname = dlg.GetDirectory()
@@ -74,11 +87,12 @@ class InpaintingGUI(wx.Frame):
         dlg.Destroy()
         
     def onOpenMask(self, e):
-        '''Open an image'''
+        '''Open mask'''
         wildcard = 'BMP files (*.bmp)|*.bmp|' +\
                    'PGM files (*.pgm)|*.pgm|' +\
                    'Other files (*.*)|*.*'
-        dlg = wx.FileDialog(self, "Choose a file", wildcard=wildcard, style=wx.OPEN)
+        dlg = wx.FileDialog(self, "Choose a file", 
+                            wildcard=wildcard, style=wx.OPEN)
         if dlg.ShowModal() == wx.ID_OK:
             self.filename = dlg.GetFilename()
             self.dirname = dlg.GetDirectory()
@@ -88,9 +102,24 @@ class InpaintingGUI(wx.Frame):
         png = png.Scale(240, 240).ConvertToBitmap()
         wx.StaticBitmap(self, -1, png, (260, 5), (240, 240))
         dlg.Destroy()
+        
+    def onPatchSize(self, e):
+        '''Dialog to set patch size'''
+        ps = 'Enter patch size value:'
+        dlg = wx.NumberEntryDialog(self, '', ps, 'Patch Size', 
+                                   self.patch_size, 1, 1000)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.patch_size = dlg.GetValue()
+        dlg.Destroy()
+        
+    def onGauss(self, e):
+        '''Configures to wheter apply Gaussian smoothing to the image 
+        prior to calculating iamge gradients.'''
+        return
     
     def onInpaint(self, e):
-        inpaint(self.img, self.mask)
+        '''Runs the inpainting algorithm when Inpaint button is clicked'''
+        inpaint(self.img, self.mask, self.gauss, self.sigma, self.patch_size)
         
 if __name__ == '__main__':
     app = wx.App(False)
